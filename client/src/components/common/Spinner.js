2fcd9c2592230dc80ed4552562c81c5087718f10
SimasDei <simondeiman@gmail.com>
2018-12-29 15:33:00 +0200

    add css spinner for loading, conditional dashboard display dineding on user profile

---
+++ client/src/components/common/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Spinner made with CSS
+const Spinner = () => {
+  return (
+    <div className="lds-ripple">
+      <div />
+      <div />
+    </div>
+  );
+};
+
+export default Spinner;
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
+import Spinner from '../common/Spinner';
+import { Link } from 'react-router-dom';
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -9,15 +11,60 @@ class Dashboard extends Component {
   }
 
   render() {
+    // Get user from the Auth State
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent;
+
+    if (profile === null || loading) {
+      dashboardContent = <Spinner />;
+    } else {
+      // Check if the current user has a Profile
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>Ahoy Sailor o/, nice Profile</h4>;
+      } else {
+        // Suggest to create a profile
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">
+              Hello, dear sir and Or mam. {user.name}
+            </p>
+            <p>Set up a Profile !</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create Profile
+            </Link>
+          </div>
+        );
+      }
+    }
     return (
-      <div style={{ height: '100vh' }}>
-        <h1>I am a dashboard</h1>
+      <div style={{ height: '100vh' }} className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">Dashbordio</h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   { getCurrentProfile }
 )(Dashboard);
