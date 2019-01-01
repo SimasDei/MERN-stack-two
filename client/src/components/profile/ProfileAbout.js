6eb4f9551146e0d6cb1fa21498c2fb8feec2e849
SimasDei <simondeiman@gmail.com>
2019-01-01 11:27:04 +0200

    add dynamic display of education and experience

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import isEmpty from '../../validation/is-empty';
+
 class ProfileAbout extends Component {
   render() {
+    const { profile } = this.props;
+
+    // Get First Name
+    const firstName = profile.user.name.trim().split(' ')[0];
+    // Map through skill array
+    const skills = profile.skills.map((skill, index) => (
+      <div className="p-3" key={index}>
+        <i className="fa fa-check" />
+        {skill}
+      </div>
+    ));
+
     return (
       <div className="row">
         <div className="col-md-12">
           <div className="card card-body bg-light mb-3">
-            <h3>Dude's bio</h3>
-            <p className="kead">Lorem ipsum dolor sit amet.</p>
+            <h3>{firstName}'s bio</h3>
+            <p className="lead">
+              {isEmpty(profile.bio) ? (
+                <span>No Bio created</span>
+              ) : (
+                <span>{profile.bio}</span>
+              )}
+            </p>
+            <hr />
+            <h3 className="text-center text-info">Acquired Skills</h3>
+            <div className="row">
+              <div className="d-flex flex-wrap justify-content-center align-items-center">
+                {skills}
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,9 +1,86 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
+
 class ProfileCreds extends Component {
   render() {
+    const { experience, education } = this.props;
+
+    // Map through experience and Education
+    const expItems = experience.map(exp => (
+      <li className="list-group-item" key={exp._id}>
+        <h4>{exp.company}</h4>
+        <p>
+          <Moment format="DD/MM/YYYY">{exp.from}</Moment>
+          {' - '}
+          {exp.to === null ? (
+            ' Present'
+          ) : (
+            <Moment format="DD/MM/YYYY">{exp.to}</Moment>
+          )}
+        </p>
+        <p>
+          <strong>Position: {exp.title}</strong>
+        </p>
+        <p>
+          {exp.location === '' ? null : (
+            <strong>Based in: {exp.location}</strong>
+          )}
+        </p>
+        <p>
+          {exp.description === '' ? null : (
+            <strong>Details: {exp.description}</strong>
+          )}
+        </p>
+      </li>
+    ));
+
+    const eduItems = education.map(edu => (
+      <li className="list-group-item" key={edu._id}>
+        <h4>{edu.school}</h4>
+        <p>
+          <Moment format="DD/MM/YYYY">{edu.from}</Moment>
+          {' - '}
+          {edu.to === null ? (
+            ' Present'
+          ) : (
+            <Moment format="DD/MM/YYYY">{edu.to}</Moment>
+          )}
+        </p>
+        <p>
+          {edu.degree === '' ? null : <strong>Degree: {edu.degree}</strong>}
+        </p>
+        <p>
+          {edu.fieldOfStudy === '' ? null : (
+            <strong>Field Of Study: {edu.fieldOfStudy}</strong>
+          )}
+        </p>
+        <p>
+          {edu.description === '' ? null : (
+            <strong>Details: {edu.description}</strong>
+          )}
+        </p>
+      </li>
+    ));
+
     return (
-      <div>
-        <h1>TODO: Profile Credentials</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Experience</h3>
+          {expItems.length > 0 ? (
+            <div className="list-group">{expItems}</div>
+          ) : (
+            <p className="text-center">Experience not provided</p>
+          )}
+        </div>
+
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Education</h3>
+          {eduItems.length > 0 ? (
+            <div className="list-group">{eduItems}</div>
+          ) : (
+            <p className="text-center">Education not provided</p>
+          )}
+        </div>
       </div>
     );
   }
