4aebae430689c8b50e73be431daaf81baaaed4f2
SimasDei <simondeiman@gmail.com>
2019-01-02 21:06:16 +0200

    try deployment #3

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 2000;
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
